e0e4eb236f6b6d5ab85d672bd35f93e1aba75a36
Jill Oakes <jericho_rules@hotmail.com>
2022-03-30 15:48:02 -0400

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+        public double Deposit(double balance)
+        {
+            _balance += balance;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+        
+        public BankAccount()
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var BankAccount = new BankAccount();
+
+            Console.WriteLine("Welcome to the First Console Bank of Indianapolis.");
+            Console.WriteLine("To see your balance, type BALANCE.  To make a deposit, type DEPOSIT: ");
+            // Convert read line to upper
+            //make a do-while loop for the correct answer
+                //if BALANCE, do a thing
+                //else DEPOSIT, do a thing
+            // while the condition is something other than balance or deposit
+            //type exit to exit?
+            
+        }
+    }
+}
