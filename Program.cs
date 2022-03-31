61c000a45a634350d024d4aec70d74e4f092a904
Jill Oakes <jericho_rules@hotmail.com>
2022-03-30 20:22:14 -0400

    Optimize code for Exercise 1

--- Program.cs
+++ Program.cs
@@ -10,80 +10,60 @@ namespace BankEncapsulation
 
             Console.WriteLine("Welcome to the First Terminal Bank of Indianapolis.");
             bool isValidResponse = false;
-            bool isDoubleResponse = false;
-            bool isDepositResponse = false;
             string userInput;
-            double userAmount;
             string userBalance = "BALANCE";
             string userDeposit = "DEPOSIT";
             string userExit = "EXIT";
-
             
             do
             {
-                Console.WriteLine("To see your balance, type BALANCE.  To make a deposit, type DEPOSIT: ");
+                Console.WriteLine("To see your balance, type BALANCE.  To make a deposit, type DEPOSIT. To exit, type EXIT: ");
                 userInput = Console.ReadLine().ToUpper();
-                if (userInput == userBalance)
+                if (userInput == userBalance || userInput == userDeposit || userInput == userExit)
                 {
                     isValidResponse = true;
-                    Console.WriteLine($"Your balance is ${account.GetBalance()}.");
-                }
-
-                if (userInput == userDeposit)
-                {
-                    isValidResponse = true;
-
-                    do
-                    {
-                        Console.Write("List the amount you wish to deposit: $");
-                        isDoubleResponse = double.TryParse(Console.ReadLine(), out userAmount);
-                    } while (isDoubleResponse != true);
-
-                    if (userAmount >= 0)
-                    {
-                        account.Deposit(userAmount);
-                        Console.WriteLine($"Your new balance is ${account.GetBalance()}");
-                        isDepositResponse = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Withdrawls are not available at this time.");
-                    }
                 }
 
             } while (isValidResponse != true);
 
 
-            while (isDepositResponse != true)
+            switch (userInput)
             {
-                Console.WriteLine("To make a deposit, type DEPOSIT. To exit, type EXIT: ");
-                userInput = Console.ReadLine().ToUpper();
-                if (userInput == userDeposit)
-                {
-                    do
-                    {
-                        Console.Write("List the amount you wish to deposit: $");
-                        isDoubleResponse = double.TryParse(Console.ReadLine(), out userAmount);
-                    } while (isDoubleResponse != true);
+                case "BALANCE":
+                    Console.WriteLine($"Your balance is ${account.GetBalance()}");
+                    Console.WriteLine("We value your business.  Don't forget to take your receipt.");
+                    break;
 
-                    if (userAmount >= 0)
-                    {
-                        account.Deposit(userAmount);
-                        Console.WriteLine($"Your new balance is ${account.GetBalance()}");
-                        isDepositResponse = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Withdrawls are not available at this time.");
-                    }
-                }
-                if (userInput == userExit)
-                {
-                    isDepositResponse = true;
-                }
+                case "DEPOSIT":
+                    Deposit(account);
+                    break;
+
+                case "EXIT":
+                    Console.WriteLine("Please come again.  Goodbye!");
+                    break;
             }
+        }
 
-            Console.WriteLine("We value your business.  Don't forget to take your receipt.");
-         }
+        private static void Deposit(BankAccount account)
+        {
+            bool isDoubleResponse = false;
+            double userAmount;
+            do
+            {
+                Console.Write("List the amount you wish to deposit: $");
+                isDoubleResponse = double.TryParse(Console.ReadLine(), out userAmount);
+            } while (isDoubleResponse != true);
+
+            if (userAmount >= 0)
+            {
+                account.Deposit(userAmount);
+                Console.WriteLine($"Your new balance is ${account.GetBalance()}");
+                Console.WriteLine("We value your business.  Don't forget to take your receipt.");
+            }
+            else
+            {
+                Console.WriteLine("Withdrawls are not available at this time.");
+            }
+        }
     }
 }
